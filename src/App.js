777adbfee3402fdd2f3ea83262d3b403e909182c
narids <nghiahoangphu@gmail.com>
2023-09-19 10:43:26 +0700

    feat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import functionComponent from './component/functionComponent';
 function App() {
   return (
     <div className="App">
---
+++ src/component/functionComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const functionComponent=()=> {
+    return(
+        <div> 
+            <h1>vi du cho component</h1>
+        </div>     
+    ) 
+}
+export default functionComponent; 
