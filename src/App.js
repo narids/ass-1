25dc3a9fe4fad0ddb003cf660817353dfcec48f7
narids <nghiahoangphu@gmail.com>
2023-09-19 11:22:30 +0700

    update functionComponent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import functionComponent from './component/functionComponent';
+import Gallery, { Profile } from './component/Gallery';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        \ Class: SE1740
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="App">
+        <functionComponent/>
+        <Gallery/> 
+        <Profile/>
+      </div>
   );
 }
 
---
+++ src/component/Gallery.js
@@ -0,0 +1,15 @@
+import React from "react";
+import slide from "../../public/images/slide_2.png";
+
+export function Profile() {
+  return <img src={slide} width={100} height={100} />;
+}
+export default function Gallery() {
+  return (
+    <section>
+        <h2>Hai anh</h2>
+        <Profile/>
+        <Profile/>
+    </section>
+  );
+}
